f499ee5483f98a1dd5155a5086b539e68535503b
Raymond Michael Morales <michaelmorales53@gmail.com>
2020-01-22 18:19:39 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,15 +1,41 @@
+require 'pp'
 
+def map(array)
 
 output = []
   
   for number in array
     
-    data = yield
-    
-    output.push(data)
+    output.push(yield(number))
   
   end
-
+  
   output
   
 end
+
+def reduce(array, start = false)
+  
+  count = 0
+  
+  if start
+    
+    result = start
+    
+  else
+    
+    result = array[0]
+    count += 1
+    
+  end
+    
+  while count < array.size do
+  
+    result = yield(result, array[count])
+    count += 1
+    
+  end
+  
+  result
+  
+end  
